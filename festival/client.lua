43e3e5642eb02a4c99b292c99e9190866111c67d
Kub1sta <kubistacze@gmail.com>
2024-07-06 17:46:49 +0200

    Update
    Added option for command

--- festival/client.lua
+++ festival/client.lua
@@ -1,40 +1,79 @@
-local enable = false
-local particle_data = {}
-
-
-RegisterCommand("startfx", function()
-    local playerPed = PlayerPedId()
-    local playerCoords = nil
-    local distance = nil
-
-    playerCoords = GetEntityCoords(playerPed)
-
-    if not enable then
-        enable = true
-
-        for k, v in pairs(config.particles) do
-            distance = #(playerCoords - v.coords)
-
-            if distance <= 100 then
-                RequestNamedPtfxAsset(v.dict)
+CreateThread(function()
+    
+    if config.command then
+        local enable = false
+        local particle_data = {}
+        
+        
+        RegisterCommand("startfx", function()
+            local playerPed = PlayerPedId()
+            local distance = nil
+            local playerCoords = nil
+            
+            playerCoords = GetEntityCoords(playerPed)
+            
+            if not enable then
+                enable = true
                 
-                while not HasNamedPtfxAssetLoaded(v.dict) do
-                    Wait(1)
+                for k, v in pairs(config.particles) do
+                    distance = #(playerCoords - v.coords)
+                    
+                    if distance <= 100 then
+                        RequestNamedPtfxAsset(v.dict)
+                        
+                        while not HasNamedPtfxAssetLoaded(v.dict) do
+                            Wait(1)
+                        end
+                        
+                        UseParticleFxAsset(v.dict)
+                        local particle = StartParticleFxLoopedAtCoord(v.name, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, v.scale, false, false, false, 0)
+                        table.insert(particle_data, particle)
+                    end
                 end
-
-                UseParticleFxAsset(v.dict)
-                local particle = StartParticleFxLoopedAtCoord(v.name, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, v.scale, false, false, false, 0)
-                table.insert(particle_data, particle)
+            else
+                
+                    enable = false
+                    for k, v in pairs(particle_data) do
+                        StopParticleFxLooped(v, false)
+                    end
+                    
+                
             end
-        end
-        return
+        end)
     else
-        if distance >= 100 then 
-            enable = false
-            for k, v in pairs(particle_data) do
-                StopParticleFxLooped(v, false)
+        local particles = {}
+        
+        CreateThread(function()
+            local playerPed = PlayerPedId()
+            local playerCoords = nil
+            while true do 
+                Wait(500)
+                playerCoords = GetEntityCoords(playerPed)
+                print(json.encode(particles))
+                for k, v in pairs(config.particles) do
+                    if #(playerCoords - v.coords) <= 100 then
+                        if not particles[v.name] then 
+                            loadParticle(v.dict)
+                            local particle_start = StartParticleFxLoopedAtCoord(v.name, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, v.scale, false, false, false, 0)
+                            particles[v.name] = particle_start
+                        end
+                    elseif #(playerCoords - v.coords) > 100 then
+                        if particles[v.name] then 
+                            StopParticleFxLooped(particles[v.name], false)
+                            particles[v.name] = nil
+                        end
+                    end
+                end
             end
-            return
+        end)
+        
+        function loadParticle(dict)
+            RequestNamedPtfxAsset(dict)
+            while not HasNamedPtfxAssetLoaded(dict) do
+                Wait(100)
+            end
+            UseParticleFxAsset(dict)
         end
-    end
+        
+    end    
 end)
--- festival/config.lua
+++ festival/config.lua
@@ -1,7 +1,8 @@
-config = {
+config = {}
 
-	particles = {
+	
+	config.particles = {
 		{dict = 'core', name = 'exp_sht_flame', scale = 5.0, coords = vector3(685.8658, 570.8864, 130.0)},
 		-- {dict = 'core', name = 'ent_amb_smoke_foundry', scale = 1.0, coords = {x = -2069.53, y = -1023.06, z = 10.00}}
 	}
-}
+	config.command = false
