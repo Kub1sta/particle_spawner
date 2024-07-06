ebf4bd780c193438640ddbcc618ddbfc18f7204b
Kub1sta <kubistacze@gmail.com>
2024-07-06 16:14:57 +0200

    init
    Edited spaces

--- festival/client.lua
+++ festival/client.lua
@@ -3,34 +3,38 @@ local particle_data = {}
 
 
 RegisterCommand("startfx", function()
-    CreateThread(function()
+    local playerPed = PlayerPedId()
+    local playerCoords = nil
+    local distance = nil
 
-            local playerPed = PlayerPedId()
-            local playerCoords = nil
-            local distance = nil
-            playerCoords = GetEntityCoords(playerPed)
-            if not enable then
-                enable = true
-                for k, v in pairs(config.particles) do
-                    distance = #(playerCoords - v.coords)
-                    if distance <= 100 then
-                        RequestNamedPtfxAsset(v.dict)
-                        while not HasNamedPtfxAssetLoaded(v.dict) do
-                            Wait(1)
-                        end
-                        UseParticleFxAsset(v.dict)
-                        local particle = StartParticleFxLoopedAtCoord(v.name, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, v.scale, false, false, false, 0)
-                        table.insert(particle_data, particle)
-                    end
+    playerCoords = GetEntityCoords(playerPed)
+
+    if not enable then
+        enable = true
+
+        for k, v in pairs(config.particles) do
+            distance = #(playerCoords - v.coords)
+
+            if distance <= 100 then
+                RequestNamedPtfxAsset(v.dict)
+                
+                while not HasNamedPtfxAssetLoaded(v.dict) do
+                    Wait(1)
                 end
-                return
-            else
-                enable = false
-                for k, v in pairs(particle_data) do
-                    StopParticleFxLooped(v, false)
-                end
-                return
+
+                UseParticleFxAsset(v.dict)
+                local particle = StartParticleFxLoopedAtCoord(v.name, v.coords.x, v.coords.y, v.coords.z, 0.0, 0.0, 0.0, v.scale, false, false, false, 0)
+                table.insert(particle_data, particle)
             end
-    end)
-
+        end
+        return
+    else
+        if distance >= 100 then 
+            enable = false
+            for k, v in pairs(particle_data) do
+                StopParticleFxLooped(v, false)
+            end
+            return
+        end
+    end
 end)
